e8742065eae11f5f40b47e8c300778b7f0ad1151
Maya Avendano <mayari.avendano@gmail.com>
2024-03-10 14:52:17 +0000

    install tailwind and basic page setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="h-screen w-full min-h-screen flex flex-col text-gray-700 bg-white">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Calculator from '$lib/components/Calculator.svelte';
+</script>
+
+<div class="h-full flex flex-col items-center justify-center">
+	<h1 class="font-semibold">whatweights.xyz</h1>
+	<p class="text-gray-400 text-sm">coming soon!</p>
+	<Calculator />
+</div>
