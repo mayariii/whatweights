50c10750f85114ea04018bc4d8fdbbbb764a341a
Maya Avendano <mayari.avendano@gmail.com>
2024-03-10 20:24:50 +0000

    add types + tidy up

--- src/lib/components/Calculator.svelte
+++ src/lib/components/Calculator.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	let barWeight = 15;
 	let addedWeight = 0;
+	let addedPlates: { colour: string; weight: number; size: string }[] = [];
 
 	$: addedPlates = [];
 	$: addedWeight = addedPlates.reduce((acc, { weight }) => acc + weight, 0);
@@ -22,44 +23,45 @@
 </script>
 
 <div class="mt-6 flex flex-col items-center gap-6">
-		<div class="flex gap-0.5 items-center min-h-32 relative">
-			<div
-				class="h-1.5 w-[350px] md:w-[400px] bg-gray-400 rounded absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-			></div>
-			{#each addedPlates
-				.slice()
-				.sort((a, b) => b.weight - a.weight)
-				.reverse() as plate}
-				<div
-					class:h-32={plate.size === 'large'}
-					class:h-20={plate.size === 'medium'}
-					class:h-16={plate.size === 'small'}
-					class:w-6={plate.size === 'large'}
-					class:w-4={plate.size !== 'large'}
-					class={`${plate.colour} rounded z-10 text-xs border border-black flex items-center justify-center`}
-				>
-					<p class:rotate-90={plate.size !== 'large'}>
-						{plate.weight}
-					</p>
-				</div>
-			{/each}
-			<div class="w-20 md:w-32"></div>
-			{#each addedPlates.slice().sort((a, b) => b.weight - a.weight) as plate}
-				<div
-					class:h-32={plate.size === 'large'}
-					class:h-20={plate.size === 'medium'}
-					class:h-16={plate.size === 'small'}
-					class:w-6={plate.size === 'large'}
-					class:w-4={plate.size !== 'large'}
-					class={`${plate.colour} rounded z-10 text-xs border border-black flex items-center justify-center`}
-				>
-					<p class:rotate-90={plate.size !== 'large'}>
-						{plate.weight}
-					</p>
-				</div>
-			{/each}
+	<div class="flex gap-0.5 items-center min-h-32 relative">
+		<div
+			class="h-1.5 w-[350px] md:w-[500px] bg-gray-400 rounded absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 		</div>
+		<!-- left side -->
+		{#each addedPlates
+			.slice()
+			.sort((a, b) => b.weight - a.weight)
+			.reverse() as plate}
+			<div
+				class:h-32={plate.size === 'large'}
+				class:h-20={plate.size === 'medium'}
+				class:h-16={plate.size === 'small'}
+				class:w-6={plate.size === 'large'}
+				class:w-4={plate.size !== 'large'}
+				class={`${plate.colour} rounded z-10 text-xs border border-black flex items-center justify-center`}>
+				<p class:rotate-90={plate.size !== 'large'}>
+					{plate.weight}
+				</p>
+			</div>
+		{/each}
 
+		<div class="w-20 md:w-32"></div>
+
+		<!-- right side -->
+		{#each addedPlates.slice().sort((a, b) => b.weight - a.weight) as plate}
+			<div
+				class:h-32={plate.size === 'large'}
+				class:h-20={plate.size === 'medium'}
+				class:h-16={plate.size === 'small'}
+				class:w-6={plate.size === 'large'}
+				class:w-4={plate.size !== 'large'}
+				class={`${plate.colour} rounded z-10 text-xs border border-black flex items-center justify-center`}>
+				<p class:rotate-90={plate.size !== 'large'}>
+					{plate.weight}
+				</p>
+			</div>
+		{/each}
+	</div>
 
 	<div class="flex flex-col gap-2 items-center">
 		<p class="text-sm text-gray-500">add plates</p>
@@ -70,6 +72,7 @@
 					class:size-20={plate.size === 'large'}
 					class:size-16={plate.size === 'medium'}
 					class:size-10={plate.size === 'small'}
+					aria-label={`add ${plate.weight}kg plates to bar`}
 					on:click={() => {
 						addedWeight = addedWeight + plate.weight;
 						platesData[platesData.indexOf(plate)].count += 2;
@@ -77,13 +80,11 @@
 							...addedPlates,
 							{ colour: plate.colour, weight: plate.weight, size: plate.size }
 						];
-					}}
-				>
+					}}>
 					{plate.weight}
 					{#if plate.count > 0}
 						<span
-							class="absolute -bottom-2 right-1/2 transform translate-x-2 bg-indigo-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs"
-						>
+							class="absolute -bottom-2 right-1/2 transform translate-x-2 bg-indigo-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
 							{plate.count}
 						</span>
 					{/if}
@@ -99,15 +100,13 @@
 				<button
 					on:click={() => (barWeight = 15)}
 					class:bg-indigo-600={barWeight === 15}
-					class:text-white={barWeight === 15}
-				>
+					class:text-white={barWeight === 15}>
 					15kg
 				</button>
 				<button
 					on:click={() => (barWeight = 20)}
 					class:bg-indigo-600={barWeight === 20}
-					class:text-white={barWeight === 20}
-				>
+					class:text-white={barWeight === 20}>
 					20kg
 				</button>
 			</div>
@@ -121,6 +120,7 @@
 		</div>
 	</div>
 
+	<!-- reset button -->
 	{#if addedWeight > 0}
 		<button
 			class="text-sm bg-gray-200 rounded px-1"
@@ -129,7 +129,6 @@
 				barWeight = 15;
 				addedPlates = [];
 				platesData.forEach((plate) => (plate.count = 0));
-			}}>reset</button
-		>
+			}}>reset</button>
 	{/if}
 </div>
